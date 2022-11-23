77d7a947416c9509ff371738e9661cce0da771d9
Sébastien Juste <juste.sebastien@proton.me>
2022-11-23 17:53:48 +0100

    feat: add view specific mail function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,17 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   
-  // Listen compose-form submitting
-  document.querySelector('#compose-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    send_email();
-    load_mailbox('sent');
-    return false;
-  });
+  // Listen inner page buttons
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
 
 /**
  * Display compose-view and undisplay emails-view
@@ -26,12 +22,14 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 }
+
 
 /**
  * Load a specific mailbox with it name
@@ -42,6 +40,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
   mailbox_name = mailbox.charAt(0).toUpperCase() + mailbox.slice(1);
  
@@ -52,10 +51,15 @@ function load_mailbox(mailbox) {
   get_mailbox_content(mailbox_name.toLowerCase());
 }
 
+
 /**
  * Use Django API to get email
+ * @param {*} event
  */
-function send_email() {
+function send_email(event) {
+  event.preventDefault()
+
+  // POST email to API route
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -65,13 +69,14 @@ function send_email() {
         body: document.querySelector('#compose-body').value
     })
   })
-  // Parse response in to JSON
-  .then(response => response.json())
+  // Load mailbox Sent
+  .then(response => load_mailbox('sent'))
   // Catch the error if one occurs
   .catch(error => {
     console.log('Error:', error);
   });
 }
+
 
 /**
  * Take a specific mailbox to render emails content
@@ -83,12 +88,11 @@ function get_mailbox_content(name) {
   fetch(`/emails/${name}`)
   .then(response => response.json())
   .then(emails => {
-      console.log(emails);
       // For each email in the array
       emails.forEach(email => {
         const element = add_html_to_element(email);
-        // Add eventListener
-        element.addEventListener('click', view_mail(email));
+        element.style.cursor = 'pointer';
+        element.addEventListener('click', () => view_mail(email.id));
         document.querySelector('#emails-view').append(element);
       });
   })
@@ -99,47 +103,83 @@ function get_mailbox_content(name) {
 
 }
 
+
 /**
  * Take an email from a list and generate html to id
  * @param {object} element
- * @return {object} button
+ * @return {object} div
  */
 function add_html_to_element(element) {
   //Create container
-  const button = document.createElement('button'); 
+  const div = document.createElement('div'); 
   // Create title
   const title = document.createElement('h6');
   title.textContent = `${element.sender}`;
-  button.appendChild(title);
+  div.appendChild(title);
   // Create and add subject
   const subject = document.createElement('p');
   subject.textContent = `${element.subject}`;
-  button.appendChild(subject);
+  div.appendChild(subject);
   // Create and add timestamp
   const timestamp = document.createElement('p');
   timestamp.textContent = `${element.timestamp}`;
-  button.appendChild(timestamp);
+  div.appendChild(timestamp);
 
-  return button
+  return div
 }
+
 
 /**
  * Get a specific email to render his page
  * @param {*} element 
  */
-function view_mail(element) {
+function view_mail(id) {
+
   // Get specific email
-  fetch(`/emails/${element.id}`)
+  fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
-      // Print email
-      console.log(email);
-
-      // ... do something else with email ...
+      // Show compose view and hide other views
+      document.querySelector('#emails-view').style.display = 'none';
+      document.querySelector('#compose-view').style.display = 'none';
+      document.querySelector('#email-view').style.display = 'block'; 
+      
+      // Display email and mark as read
+      markAsRead(email);
+      displayEmail(email);
+      
   })
   // Catch potential error
   .catch(error => {
     console.log('Error: ', error);
   });
 
+}
+
+
+/**
+ * Complete email fields in inbox.html
+ * @param {*} email 
+ */
+function displayEmail(email) {
+  document.querySelector('#email-sender').innerHTML = `<em>From:</em> ${email.sender}`;
+  document.querySelector('#email-recipients').innerHTML = `<em>To:</em> ${email.recipients}`;
+  document.querySelector('#email-subject').innerHTML = `<em>Subject:</em> ${email.subject}`;
+  document.querySelector('#email-timestamp').innerHTML = `${email.timestamp}`;
+  document.querySelector('#email-body').innerHTML = `${email.body}`;
+}
+
+
+/**
+ * Mark an email to read statement
+ * @param {*} email
+ */
+function markAsRead(email) {
+  fetch(`/emails/${email.id}` , {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  });
+  
 }
